bde5309559b2dfcad5402c7c790447645fd2d0be
32chens <647854697@qq.com>
2019-07-09 15:56:34 +0800

    Json数据转换

--- src/com/ssm/controller/UserController.java
+++ src/com/ssm/controller/UserController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ssm.pojo.Orders;
 import com.ssm.pojo.User;
@@ -92,5 +96,46 @@ public class UserController {
             return "seccess";
     }
 
-
+	
+	
+	//转向json页面
+		@RequestMapping("/toJson")
+		public String toJson() {
+			return "json";
+		}
+		
+		//转向restful页面
+		@RequestMapping("/toRestful")
+		public String toRestful() {
+			return "restful";
+		}
+		
+		
+		/**
+		 * @return
+		 * 接收页面请求的JSON数据，并返回JSON格式结果
+		 */
+		@RequestMapping("/testJson")
+		@ResponseBody
+		public User testJson(@RequestBody User user) {
+			System.out.println(user);
+			return user;
+		}
+		
+		/**
+		 * @return
+		 *  接收RESTful风格的请求，其接收方式为GET
+		 */
+		@RequestMapping(value="/user/{id}",method=RequestMethod.GET)
+		@ResponseBody
+		public User selectUser(@PathVariable("id") String id) {
+			System.out.println("id:"+id);
+			User user = new User();
+			if(id.equals("1234")) {
+				user.setUsername("tom");
+			}
+			return user;
+		}
+	
+	
 }
